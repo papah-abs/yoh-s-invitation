1f4793513f3396fbebe542f75c333d641a984cd3
papah-abs <abdelazizdon9@gmail.com>
2024-11-20 00:30:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("dateForm").onsubmit = function(event) {
+    event.preventDefault(); // Prevent form submission
+    alert("Thanks for filling out the form! I'll make sure to plan the best date based on your answers.");
+};
